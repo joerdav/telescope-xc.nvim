4fa968dfef3d13350a37f2eaae7c536aaf378dd8
joerdav <joe.davidson.21111@gmail.com>
2023-11-20 09:52:43 +0000

    wip: add empty config func

--- lua/_extensions/xc.lua
+++ lua/_extensions/xc.lua
@@ -1,13 +1,16 @@
 local has_telescope, telescope = pcall(require, "telescope")
 
 if not has_telescope then
-  error "This plugin requires telescope.nvim (https://github.com/nvim-telescope/telescope.nvim)"
+	error "This plugin requires telescope.nvim (https://github.com/nvim-telescope/telescope.nvim)"
 end
 
 local xc = require("xc")
 
 return telescope.register_extension {
-  exports = {
-    run_task = xc.run_task,
-  },
+	config = function()
+
+	end,
+	exports = {
+		run_task = xc.run_task,
+	},
 }
